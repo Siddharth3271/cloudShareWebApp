c5986a892067ddde404d65f4d42f557fc9e5c6e6
Siddharth Shukla <formaledusiddh371@gmail.com>
2025-10-03 11:05:06 +0530

    folder changed

---
+++ ShareSphereBackend/src/main/java/com/siddh/ShareSphereBackend/ShareSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siddh.ShareSphereBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareSphereBackendApplication.class, args);
+	}
+
+}
---
+++ ShareSphereBackend/src/test/java/com/siddh/ShareSphereBackend/ShareSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddh.ShareSphereBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
